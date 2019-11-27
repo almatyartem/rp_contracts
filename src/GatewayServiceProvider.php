9aedf6a2f9610e281933bf0acc172aa7a750f6ef
Artem Golodov <almatyartem@gmail.com>
2019-11-27 23:42:02 +0600

    save

--- src/GatewayServiceProvider.php
+++ src/GatewayServiceProvider.php
@@ -13,7 +13,13 @@ class GatewayServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->when(GatewayApi::class)
+        $this->app->singleton(GatewayApi::class, function ($app)
+        {
+            return new GatewayApi($app->make(Client::class), env('GATEWAY_API_URL'), env('GATEWAY_API_ENV'),
+                env('GATEWAY_API_CLIENT_ID'), env('GATEWAY_API_CLIENT_SECRET'), env('APP_ENV') == 'local');
+        });
+
+        /*$this->app->when(GatewayApi::class)
             ->needs('$endpoint')
             ->give(env('GATEWAY_API_URL'));
 
@@ -31,6 +37,6 @@ class GatewayServiceProvider extends ServiceProvider
 
         $this->app->when(GatewayApi::class)
             ->needs('$isDebug')
-            ->give(env('APP_ENV') == 'local');
+            ->give(env('APP_ENV') == 'local');*/
     }
 }
