e59c9753b26959d171d7e2253957953fcca4da02
Artem Golodov <almatyartem@gmail.com>
2019-11-30 14:28:25 +0600

    save

--- src/GatewayServiceProvider.php
+++ src/GatewayServiceProvider.php
@@ -13,60 +13,18 @@ class GatewayServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->when(GatewayApi::class)
-            ->needs('$endpoint')
-            ->give(function(){
-                return env('GATEWAY_API_URL');
-            });
+        $this->app->singleton('ApiSdk\GatewayApi', function ($app) {
+            return new GatewayApi($app->make(GuzzleHttp\Client::class), env('GATEWAY_API_URL'), env('GATEWAY_API_ENV'), env('GATEWAY_API_APP'),
+                env('GATEWAY_API_APP_TOKEN'), env('APP_ENV')=='local');
+        });
 
-        $this->app->when(GatewayApi::class)
-            ->needs('$env')
-            ->give(function(){
-                return env('GATEWAY_API_ENV');
-            });
+        $this->app->singleton('ApiSdk\AuthApi', function ($app) {
+            return new AuthApi($app->make(GatewayApi::class), env('AUTH_API_CLIENT_ID'), env('AUTH_API_CLIENT_SECRET'), env('APP_URL').'/oauth_callback');
+        });
 
-        $this->app->when(GatewayApi::class)
-            ->needs('$app')
-            ->give(function(){
-                return env('GATEWAY_API_APP');
-            });
-
-        $this->app->when(GatewayApi::class)
-            ->needs('$token')
-            ->give(function(){
-                return env('GATEWAY_API_APP_TOKEN');
-            });
-
-        $this->app->when(GatewayApi::class)
-            ->needs('$isDebug')
-            ->give(function(){
-                return env('APP_ENV');
-            });
-
-        $this->app->when(StructureApi::class)
-            ->needs('$structure')
-            ->give(function(){
-                return json_decode(file_get_contents(base_path('config/structure.json')), true);
-            });
-
-        $this->app->when(AuthApi::class)
-            ->needs('$clientId')
-            ->give(function(){
-                return env('AUTH_API_CLIENT_ID');
-            });
-
-        $this->app->when(AuthApi::class)
-            ->needs('$clientSecret')
-            ->give(function(){
-                return env('AUTH_API_CLIENT_SECRET');
-            });
-
-        $this->app->when(AuthApi::class)
-            ->needs('$oauthCallback')
-            ->give(function(){
-                return env('APP_URL').'/oauth_callback';
-            });
-
+        $this->app->singleton('ApiSdk\StructureApi', function ($app) {
+            return new StructureApi(json_decode(file_get_contents(base_path('config/structure.json')), true));
+        });
 
         $this->app->bind('coreapi',function(){
             return $this->app->make(CoreApi::class);
