90d59af77ee360e91d2a11a394c9c3c03d59539c
Artem Golodov <almatyartem@gmail.com>
2020-01-21 17:42:46 +0600

    save

--- src/GatewayServiceProvider.php
+++ src/GatewayServiceProvider.php
@@ -22,16 +22,8 @@ class GatewayServiceProvider extends ServiceProvider
             return new AuthApi($app->make('ApiSdk\GatewayApi'), env('AUTH_API_CLIENT_ID'), env('AUTH_API_CLIENT_SECRET'), env('APP_URL').'/oauth_callback');
         });
 
-        $this->app->singleton('ApiSdk\StructureApi', function ($app) {
-            return new StructureApi(json_decode(file_get_contents(base_path('config/structure.json')), true));
-        });
-
         $this->app->bind('coreapi',function($app){
             return $app->make('ApiSdk\CoreApi');
-        });
-
-        $this->app->bind('structureapi',function($app){
-            return $app->make('ApiSdk\StructureApi');
         });
 
         $this->app->bind('reportsapi',function($app){
