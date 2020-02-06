9593af3b8a69760150e3f311a9eec135093ad1fd
Artem Golodov <almatyartem@gmail.com>
2020-02-06 13:56:44 +0600

    save

--- src/GatewayServiceProvider.php
+++ src/GatewayServiceProvider.php
@@ -13,36 +13,29 @@ class GatewayServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($apiUrl = env('GATEWAY_API_URL') and $apiEnv = env('GATEWAY_API_ENV') and $apiApp = env('GATEWAY_API_APP') and $apiToken = env('GATEWAY_API_APP_TOKEN'))
-        {
-            $this->app->singleton('ApiSdk\GatewayApi', function($app) use ($apiUrl, $apiEnv, $apiApp, $apiToken)
-            {
-                return new GatewayApi($app->make('GuzzleHttp\Client'), $apiUrl, $apiEnv, $apiApp, $apiToken, env('APP_ENV') == 'local');
-            });
+        $this->app->singleton('ApiSdk\GatewayApi', function ($app) {
+            return new GatewayApi($app->make('GuzzleHttp\Client'), env('GATEWAY_API_URL'), env('GATEWAY_API_ENV'), env('GATEWAY_API_APP'),
+                env('GATEWAY_API_APP_TOKEN'), env('APP_ENV')=='local');
+        });
 
-            if($authClientId = env('AUTH_API_CLIENT_ID') and $authClientSecret = env('AUTH_API_CLIENT_SECRET'))
-            {
-                $this->app->singleton('ApiSdk\AuthApi', function($app) use ($authClientId, $authClientSecret)
-                {
-                    return new AuthApi($app->make('ApiSdk\GatewayApi'), $authClientId, $authClientSecret, env('APP_URL') . '/oauth_callback');
-                });
-            }
+        $this->app->singleton('ApiSdk\AuthApi', function ($app) {
+            return new AuthApi($app->make('ApiSdk\GatewayApi'), env('AUTH_API_CLIENT_ID'), env('AUTH_API_CLIENT_SECRET'), env('APP_URL').'/oauth_callback');
+        });
 
-            $this->app->bind('coreapi',function($app){
-                return $app->make('ApiSdk\CoreApi');
-            });
+        $this->app->bind('coreapi',function($app){
+            return $app->make('ApiSdk\CoreApi');
+        });
 
-            $this->app->bind('reportsapi',function($app){
-                return $app->make('ApiSdk\ReportsApi');
-            });
+        $this->app->bind('reportsapi',function($app){
+            return $app->make('ApiSdk\ReportsApi');
+        });
 
-            $this->app->bind('filesapi',function($app){
-                return $app->make('ApiSdk\FilesApi');
-            });
+        $this->app->bind('filesapi',function($app){
+            return $app->make('ApiSdk\FilesApi');
+        });
 
-            $this->app->bind('authapi',function($app){
-                return $app->make('ApiSdk\AuthApi');
-            });
-        }
+        $this->app->bind('authapi',function($app){
+            return $app->make('ApiSdk\AuthApi');
+        });
     }
 }
