e0f76cad37c496b81b72d70e4e960abc41d01b49
Artem Golodov <almatyartem@gmail.com>
2020-02-19 23:10:59 +0600

    refactoring

--- src/ApiSdkServiceProvider.php
+++ src/ApiSdkServiceProvider.php
@@ -19,18 +19,26 @@ class ApiSdkServiceProvider extends ServiceProvider
                 env('GATEWAY_API_APP_TOKEN'), env('APP_ENV')=='local');
         });
 
-        $this->app->singleton('ApiSdk\DirectRequestProvider', function ($app) {
-            return new DirectRequestProvider($app->make('GuzzleHttp\Client'), env('APP_ENV')=='local');
-        });
-
         $this->app->singleton('ApiSdk\AuthApi', function ($app) {
             return new AuthApi($app->make('ApiSdk\GatewayRequestProvider'), env('AUTH_API_CLIENT_ID'), env('AUTH_API_CLIENT_SECRET'),
-                env('APP_URL').'/oauth_callback', env('GATEWAY_API_ENV'), env('GATEWAY_API_APP'));
+                env('APP_URL').'/oauth_callback', env('GATEWAY_API_ENV'), env('GATEWAY_API_APP'), env('AUTHAPI_ENDPOINT'));
         });
 
         $this->app->bind('ApiSdk\Contracts\RequestProvider',function($app){
             return env('GATEWAY_API_URL') ? $app->make('ApiSdk\GatewayApi') : $app->make('ApiSdk\DirectRequestProvider');
         });
+
+        $this->app->when('ApiSdk\DirectRequestProvider')
+            ->needs('$isDebug')
+            ->give(env('APP_ENV')=='local');
+
+        $this->app->when('ApiSdk\CoreApi')
+            ->needs('$api')
+            ->give(env('COREAPI_ENDPOINT'));
+
+        $this->app->when('ApiSdk\FilesApi')
+            ->needs('$api')
+            ->give(env('FILESAPI_ENDPOINT') ? env('FILESAPI_ENDPOINT').'/'.env('GATEWAY_API_ENV') : null);
 
         $this->app->bind('coreapi',function($app){
             return $app->make('ApiSdk\CoreApi');
