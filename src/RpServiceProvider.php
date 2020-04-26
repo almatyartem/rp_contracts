786fbafeccdfa0ac4b001e7dbe177de83408591b
Artem Golodov <almatyartem@gmail.com>
2020-04-26 20:51:11 +0600

    save

---
+++ src/RpServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ApiSdk;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class RpServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('ApiSdk\Contracts\RequestProvider',function(Application $app){
+            return env('GATEWAY_API_URL') ? $app->make('ApiSdk\GatewayApi') : $app->make('ApiSdk\DirectRequestProvider');
+        });
+
+        $this->app->when('ApiSdk\DirectRequestProvider')
+            ->needs('$isDebug')
+            ->give(env('APP_ENV')=='local');
+
+        $this->app->when('ApiSdk\GatewayRequestProvider')
+            ->needs('$endpoint')
+            ->give(env('GATEWAY_API_URL'));
+
+        $this->app->when('ApiSdk\GatewayRequestProvider')
+            ->needs('$env')
+            ->give(env('GATEWAY_API_ENV'));
+
+        $this->app->when('ApiSdk\GatewayRequestProvider')
+            ->needs('$app')
+            ->give(env('APP_ENV')=='local');
+
+        $this->app->when('ApiSdk\GatewayRequestProvider')
+            ->needs('$token')
+            ->give(env('GATEWAY_API_APP'));
+
+        $this->app->when('ApiSdk\GatewayRequestProvider')
+            ->needs('$isDebug')
+            ->give(env('GATEWAY_API_APP_TOKEN'));
+    }
+}
